f3a1f4eb6b7c1587e5ecfc5a1481063f8b3c72c0
eelias13 <maierelias13@gmai.com>
2024-12-27 12:59:32 +0100

    add reporting to all

--- src/demiurge/generic-loop-functions/Missions/AggregationXORMission.cpp
+++ src/demiurge/generic-loop-functions/Missions/AggregationXORMission.cpp
@@ -3,7 +3,8 @@
 /****************************************/
 /****************************************/
 
-AggregationXORMission::AggregationXORMission() {
+AggregationXORMission::AggregationXORMission()
+{
   m_strMissionType = "aggXOR";
   m_fCurrentTimeScore = 0.0f;
   m_fAnyTimeScore = 0.0f;
@@ -14,67 +15,77 @@ AggregationXORMission::AggregationXORMission() {
 /****************************************/
 /****************************************/
 
-AggregationXORMission::AggregationXORMission(const AggregationXORMission& orig) {
+AggregationXORMission::AggregationXORMission(const AggregationXORMission &orig)
+{
 }
 
 /****************************************/
 /****************************************/
 
-void AggregationXORMission::Init(TConfigurationNode& t_tree) {
+void AggregationXORMission::Init(TConfigurationNode &t_tree)
+{
   m_bInitializedByARGoS = true;
 
   GenericLoopFunctions::Init(t_tree);
 
   std::vector<std::string>::iterator it;
 
-  try {
+  try
+  {
     it = std::find(m_vecMissionDescription.begin(), m_vecMissionDescription.end(), "--colhome");
-    m_cColorHome.Set((*(it+1)).c_str());
+    m_cColorHome.Set((*(it + 1)).c_str());
 
     it = std::find(m_vecMissionDescription.begin(), m_vecMissionDescription.end(), "--coldistraction");
-    m_cColorDistractions.Set((*(it+1)).c_str());
+    m_cColorDistractions.Set((*(it + 1)).c_str());
 
     it = std::find(m_vecMissionDescription.begin(), m_vecMissionDescription.end(), "--tof");
-    m_strTypeObjectiveFunction = (*(it+1)).c_str();
+    m_strTypeObjectiveFunction = (*(it + 1)).c_str();
 
     // Initializing the map containing the number of robots on each home
     it = std::find(m_vecMissionDescription.begin(), m_vecMissionDescription.end(), "--nh");
-    m_unNumberHomes = atoi((*(it+1)).c_str());
-    for (UInt16 i = 0; i < m_unNumberHomes; i++) {
+    m_unNumberHomes = atoi((*(it + 1)).c_str());
+    for (UInt16 i = 0; i < m_unNumberHomes; i++)
+    {
       m_mapHomesData[i] = 0;
     }
     std::cout << "Ok" << std::endl;
 
-
     it = std::find(m_vecMissionDescription.begin(), m_vecMissionDescription.end(), "--np");
-    UInt16 unNumberPatches = atoi((*(it+1)).c_str());
+    UInt16 unNumberPatches = atoi((*(it + 1)).c_str());
     std::vector<std::string>::iterator vecFirstPatch;
     std::vector<std::string>::iterator vecSecondPatch;
-    for (size_t i = 0; i < unNumberPatches; i++) {
-      Patch* pcCurrentPatch;
+    for (size_t i = 0; i < unNumberPatches; i++)
+    {
+      Patch *pcCurrentPatch;
       std::ostringstream oss;
       oss << "--p" << i;
       vecFirstPatch = std::find(m_vecMissionDescription.begin(), m_vecMissionDescription.end(), oss.str());
-      if (i+1 < unNumberPatches) {
+      if (i + 1 < unNumberPatches)
+      {
         std::ostringstream oss;
-        oss << "--p" << i+1;
+        oss << "--p" << i + 1;
         vecSecondPatch = std::find(m_vecMissionDescription.begin(), m_vecMissionDescription.end(), oss.str());
-      } else {
+      }
+      else
+      {
         vecSecondPatch = m_vecMissionDescription.end();
       }
       std::vector<std::string> vecPatchConfig(vecFirstPatch, vecSecondPatch);
       pcCurrentPatch = HandlePatch(vecPatchConfig, i);
       m_vecAllPatches.push_back(pcCurrentPatch);
-      if (pcCurrentPatch->GetColor() == m_cColorHome) {
+      if (pcCurrentPatch->GetColor() == m_cColorHome)
+      {
         m_vecHomes.push_back(pcCurrentPatch);
-      } else {
+      }
+      else
+      {
         m_vecDistractionPatches.push_back(pcCurrentPatch);
       }
       std::cout << "Ok" << i << std::endl;
-
     }
-
-  } catch (std::exception e) {
+  }
+  catch (std::exception e)
+  {
     LOGERR << "Error while parsing mission description: " << e.what() << std::endl;
   }
 
@@ -84,12 +95,16 @@ void AggregationXORMission::Init(TConfigurationNode& t_tree) {
 /****************************************/
 /****************************************/
 
-AggregationXORMission::~AggregationXORMission() {
-  if (m_bInitializedByARGoS) {
-    for (size_t i = 0; i < m_vecHomes.size(); i++) {
+AggregationXORMission::~AggregationXORMission()
+{
+  if (m_bInitializedByARGoS)
+  {
+    for (size_t i = 0; i < m_vecHomes.size(); i++)
+    {
       delete m_vecHomes[i];
     }
-    for (size_t i = 0; i < m_vecDistractionPatches.size(); i++) {
+    for (size_t i = 0; i < m_vecDistractionPatches.size(); i++)
+    {
       delete m_vecDistractionPatches[i];
     }
   }
@@ -103,19 +118,21 @@ void AggregationXORMission::Destroy() {}
 /****************************************/
 /****************************************/
 
-argos::CColor AggregationXORMission::GetFloorColor(const argos::CVector2& c_position_on_plane) {
+argos::CColor AggregationXORMission::GetFloorColor(const argos::CVector2 &c_position_on_plane)
+{
   return GenericLoopFunctions::GetFloorColor(c_position_on_plane);
 }
 
-
 /****************************************/
 /****************************************/
 
-void AggregationXORMission::Reset() {
+void AggregationXORMission::Reset()
+{
   GenericLoopFunctions::Reset();
   m_fCurrentTimeScore = 0.0f;
   m_fAnyTimeScore = 0.0f;
-  for (UInt16 i = 0; i < m_unNumberHomes; i++) {
+  for (UInt16 i = 0; i < m_unNumberHomes; i++)
+  {
     m_mapHomesData[i] = 0;
   }
 }
@@ -123,24 +140,34 @@ void AggregationXORMission::Reset() {
 /****************************************/
 /****************************************/
 
-void AggregationXORMission::PostStep() {
+void AggregationXORMission::PostStep()
+{
   std::map<UInt16, UInt32> mapScoreTemp = m_mapHomesData;
-  for (UInt16 i = 0; i < m_unNumberHomes; i++) {
+  for (UInt16 i = 0; i < m_unNumberHomes; i++)
+  {
     m_mapHomesData[i] = 0;
   }
 
-  CSpace::TMapPerType& tEpuckMap = GetSpace().GetEntitiesByType("epuck");
-  CVector2 cEpuckPosition(0,0);
-  for (CSpace::TMapPerType::iterator it = tEpuckMap.begin(); it != tEpuckMap.end(); ++it) {
-    CEPuckEntity* pcEpuck = any_cast<CEPuckEntity*>(it->second);
+  CSpace::TMapPerType &tEpuckMap = GetSpace().GetEntitiesByType("epuck");
+  CVector2 cEpuckPosition(0, 0);
+  for (CSpace::TMapPerType::iterator it = tEpuckMap.begin(); it != tEpuckMap.end(); ++it)
+  {
+    CEPuckEntity *pcEpuck = any_cast<CEPuckEntity *>(it->second);
 
     std::string strRobotId = pcEpuck->GetId();
     cEpuckPosition.Set(pcEpuck->GetEmbodiedEntity().GetOriginAnchor().Position.GetX(),
                        pcEpuck->GetEmbodiedEntity().GetOriginAnchor().Position.GetY());
 
+    std::cout << "%! id:" << count;
+    std::cout << " x:" << pcEpuck->GetEmbodiedEntity().GetOriginAnchor().Position.GetX();
+    std::cout << " y:" << pcEpuck->GetEmbodiedEntity().GetOriginAnchor().Position.GetY() << std::endl;
+    count++;
+
     // Check if robot on any of the home
-    for (std::vector<Patch*>::iterator itH = m_vecHomes.begin(); itH != m_vecHomes.end(); ++itH) {
-      if ((*itH)->IsOnPatch(cEpuckPosition)) {
+    for (std::vector<Patch *>::iterator itH = m_vecHomes.begin(); itH != m_vecHomes.end(); ++itH)
+    {
+      if ((*itH)->IsOnPatch(cEpuckPosition))
+      {
         m_mapHomesData[(*itH)->GetIndex()] += 1;
       }
     }
@@ -148,17 +175,20 @@ void AggregationXORMission::PostStep() {
 
   m_fCurrentTimeScore = LargestGroupOfRobotsInOneHome() / Real(m_unNumberRobots);
   m_fAnyTimeScore += m_fCurrentTimeScore;
-
 }
 
 /****************************************/
 /****************************************/
 
-Real AggregationXORMission::GetScore() {
+Real AggregationXORMission::GetScore()
+{
   Real fScore = 0.0f;
-  if (m_strTypeObjectiveFunction == "'anytime'") {
+  if (m_strTypeObjectiveFunction == "'anytime'")
+  {
     fScore = m_fAnyTimeScore;
-  } else if (m_strTypeObjectiveFunction == "'endtime'") {
+  }
+  else if (m_strTypeObjectiveFunction == "'endtime'")
+  {
     fScore = m_fCurrentTimeScore;
   }
   return fScore;
@@ -167,14 +197,16 @@ Real AggregationXORMission::GetScore() {
 /****************************************/
 /****************************************/
 
-void AggregationXORMission::PostExperiment() {
+void AggregationXORMission::PostExperiment()
+{
   LOG << "Score " << GetScore() << std::endl;
 }
 
 /****************************************/
 /****************************************/
 
-void AggregationXORMission::Initialize() {
+void AggregationXORMission::Initialize()
+{
   GenericLoopFunctions::Initialize();
 
   std::map<std::string, argos::CAny>::iterator it;
@@ -184,13 +216,18 @@ void AggregationXORMission::Initialize() {
 
   it = GetVariableFromDictionary("colorHome");
   std::string strColorHome = argos::any_cast<std::string>(it->second);
-  if (strColorHome == "'white'") {
+  if (strColorHome == "'white'")
+  {
     m_cColorHome = argos::CColor::WHITE;
     m_cColorDistractions = argos::CColor::BLACK;
-  } else if (strColorHome == "'black'") {
+  }
+  else if (strColorHome == "'black'")
+  {
     m_cColorHome = argos::CColor::BLACK;
     m_cColorDistractions = argos::CColor::WHITE;
-  } else {
+  }
+  else
+  {
     std::cout << "\033[1;31m" << "Error: unknown color nest \"" << strColorHome << "\" ." << "\033[0m" << std::endl;
     std::exit(1);
   }
@@ -202,18 +239,22 @@ void AggregationXORMission::Initialize() {
   unsigned int unNumberDistractions = stoi(argos::any_cast<std::string>(it->second));
   unsigned int iPatch = 0;
 
-  std::vector<Patch*> vecPossibleAggregationPatches = m_pcArena->GetPossibleAggregationPatches();
-  std::vector<Patch*> vecAllPossiblePatches = m_pcArena->GetAllPossiblePatches();
+  std::vector<Patch *> vecPossibleAggregationPatches = m_pcArena->GetPossibleAggregationPatches();
+  std::vector<Patch *> vecAllPossiblePatches = m_pcArena->GetAllPossiblePatches();
   unsigned int unRandomIndex;
 
-  while (iPatch < (m_unNumberHomes + unNumberDistractions)) {
-    Patch* pcNewPatch;
-    if (iPatch < m_unNumberHomes) {
+  while (iPatch < (m_unNumberHomes + unNumberDistractions))
+  {
+    Patch *pcNewPatch;
+    if (iPatch < m_unNumberHomes)
+    {
       unRandomIndex = rand() % vecPossibleAggregationPatches.size();
       pcNewPatch = vecPossibleAggregationPatches[unRandomIndex];
       pcNewPatch->SetIndex(m_vecAllPatches.size());
       pcNewPatch->SetColor(m_cColorHome);
-    } else {
+    }
+    else
+    {
       unRandomIndex = rand() % vecAllPossiblePatches.size();
       pcNewPatch = vecAllPossiblePatches[unRandomIndex];
       pcNewPatch->SetIndex(m_vecAllPatches.size());
@@ -221,12 +262,16 @@ void AggregationXORMission::Initialize() {
     }
 
     // Add patch to list of patches if no overlap with existing ones
-    if (!(m_pcArena->ArePatchesOverlapping(m_vecAllPatches, pcNewPatch))) {
+    if (!(m_pcArena->ArePatchesOverlapping(m_vecAllPatches, pcNewPatch)))
+    {
       m_vecAllPatches.push_back(pcNewPatch);
-      if (iPatch < m_unNumberHomes) {
+      if (iPatch < m_unNumberHomes)
+      {
         m_vecHomes.push_back(pcNewPatch);
         vecPossibleAggregationPatches.erase(vecPossibleAggregationPatches.begin() + unRandomIndex);
-      } else {
+      }
+      else
+      {
         m_vecDistractionPatches.push_back(pcNewPatch);
         vecAllPossiblePatches.erase(vecAllPossiblePatches.begin() + unRandomIndex);
       }
@@ -234,13 +279,13 @@ void AggregationXORMission::Initialize() {
       iPatch += 1;
     }
   }
-
 }
 
 /****************************************/
 /****************************************/
 
-std::string AggregationXORMission::GetLowLevelDescription() {
+std::string AggregationXORMission::GetLowLevelDescription()
+{
   std::string strGenericPart = GenericLoopFunctions::GetLowLevelDescription();
   std::ostringstream cStream;
   cStream << strGenericPart;
@@ -249,7 +294,8 @@ std::string AggregationXORMission::GetLowLevelDescription() {
   cStream << "--coldistraction " << m_cColorDistractions << " ";
   cStream << "--nh " << m_unNumberHomes << " ";
   cStream << "--np " << m_vecAllPatches.size() << " ";
-  for (size_t i = 0; i < m_vecAllPatches.size(); ++i) {
+  for (size_t i = 0; i < m_vecAllPatches.size(); ++i)
+  {
     cStream << m_vecAllPatches[i]->GetLowLevelDescription();
   }
   return cStream.str();
@@ -258,10 +304,13 @@ std::string AggregationXORMission::GetLowLevelDescription() {
 /****************************************/
 /****************************************/
 
-float AggregationXORMission::GetMaxValue(std::vector<float> vec_values) {
+float AggregationXORMission::GetMaxValue(std::vector<float> vec_values)
+{
   float fLargestElement = vec_values[0];
-  for (std::vector<float>::iterator it = vec_values.begin(); it < vec_values.end(); it++) {
-    if (*it > fLargestElement) {
+  for (std::vector<float>::iterator it = vec_values.begin(); it < vec_values.end(); it++)
+  {
+    if (*it > fLargestElement)
+    {
       fLargestElement = *it;
     }
   }
@@ -271,10 +320,13 @@ float AggregationXORMission::GetMaxValue(std::vector<float> vec_values) {
 /****************************************/
 /****************************************/
 
-UInt32 AggregationXORMission::SmallestGroupOfRobotsInOneHome() {
+UInt32 AggregationXORMission::SmallestGroupOfRobotsInOneHome()
+{
   UInt32 unSmallestGroup = m_unNumberRobots;
-  for (UInt16 i = 0; i < m_unNumberHomes; i++) {
-    if (m_mapHomesData[i] < unSmallestGroup) {
+  for (UInt16 i = 0; i < m_unNumberHomes; i++)
+  {
+    if (m_mapHomesData[i] < unSmallestGroup)
+    {
       unSmallestGroup = m_mapHomesData[i];
     }
   }
@@ -284,10 +336,13 @@ UInt32 AggregationXORMission::SmallestGroupOfRobotsInOneHome() {
 /****************************************/
 /****************************************/
 
-UInt32 AggregationXORMission::LargestGroupOfRobotsInOneHome() {
+UInt32 AggregationXORMission::LargestGroupOfRobotsInOneHome()
+{
   UInt32 unLargestGroup = 0;
-  for (UInt16 i = 0; i < m_unNumberHomes; i++) {
-    if (m_mapHomesData[i] > unLargestGroup) {
+  for (UInt16 i = 0; i < m_unNumberHomes; i++)
+  {
+    if (m_mapHomesData[i] > unLargestGroup)
+    {
       unLargestGroup = m_mapHomesData[i];
     }
   }
--- src/demiurge/generic-loop-functions/Missions/HomingMission.cpp
+++ src/demiurge/generic-loop-functions/Missions/HomingMission.cpp
@@ -3,7 +3,8 @@
 /****************************************/
 /****************************************/
 
-HomingMission::HomingMission() {
+HomingMission::HomingMission()
+{
   m_strMissionType = "homing";
   m_strTypeObjectiveFunction = "";
   m_unRobotsAtHome = 0;
@@ -15,55 +16,67 @@ HomingMission::HomingMission() {
 /****************************************/
 /****************************************/
 
-HomingMission::HomingMission(const HomingMission& orig) {
+HomingMission::HomingMission(const HomingMission &orig)
+{
 }
 
 /****************************************/
 /****************************************/
 
-void HomingMission::Init(TConfigurationNode& t_tree) {
+void HomingMission::Init(TConfigurationNode &t_tree)
+{
   m_bInitializedByARGoS = true;
 
   GenericLoopFunctions::Init(t_tree);
 
   std::vector<std::string>::iterator it;
 
-  try {
+  try
+  {
     it = std::find(m_vecMissionDescription.begin(), m_vecMissionDescription.end(), "--colhome");
-    m_cColorHome.Set((*(it+1)).c_str());
+    m_cColorHome.Set((*(it + 1)).c_str());
 
     it = std::find(m_vecMissionDescription.begin(), m_vecMissionDescription.end(), "--coldistration");
-    m_cColorDistractions.Set((*(it+1)).c_str());
+    m_cColorDistractions.Set((*(it + 1)).c_str());
 
     it = std::find(m_vecMissionDescription.begin(), m_vecMissionDescription.end(), "--tof");
-    m_strTypeObjectiveFunction = (*(it+1)).c_str();
+    m_strTypeObjectiveFunction = (*(it + 1)).c_str();
 
     it = std::find(m_vecMissionDescription.begin(), m_vecMissionDescription.end(), "--np");
-    UInt16 unNumberPatches = atoi((*(it+1)).c_str());
+    UInt16 unNumberPatches = atoi((*(it + 1)).c_str());
     std::vector<std::string>::iterator vecFirstPatch;
     std::vector<std::string>::iterator vecSecondPatch;
-    for (size_t i = 0; i < unNumberPatches; i++) {
-      Patch* pcCurrentPatch;
+    for (size_t i = 0; i < unNumberPatches; i++)
+    {
+      Patch *pcCurrentPatch;
       std::ostringstream oss;
       oss << "--p" << i;
       vecFirstPatch = std::find(m_vecMissionDescription.begin(), m_vecMissionDescription.end(), oss.str());
-      if (i+1 < unNumberPatches) {
+      if (i + 1 < unNumberPatches)
+      {
         std::ostringstream oss;
-        oss << "--p" << i+1;
+        oss << "--p" << i + 1;
         vecSecondPatch = std::find(m_vecMissionDescription.begin(), m_vecMissionDescription.end(), oss.str());
-      } else {
+      }
+      else
+      {
         vecSecondPatch = m_vecMissionDescription.end();
       }
       std::vector<std::string> vecPatchConfig(vecFirstPatch, vecSecondPatch);
       pcCurrentPatch = HandlePatch(vecPatchConfig, i);
       m_vecAllPatches.push_back(pcCurrentPatch);
-      if (pcCurrentPatch->GetColor() == m_cColorHome) {
+      if (pcCurrentPatch->GetColor() == m_cColorHome)
+      {
         m_pcHome = pcCurrentPatch;
-      } else {
+      }
+      else
+      {
         m_vecDistractionPatches.push_back(pcCurrentPatch);
       }
     }
-  } catch (std::exception e) {
+  }
+  catch (std::exception e)
+  {
     LOGERR << "Error while parsing mission description: " << e.what() << std::endl;
   }
 
@@ -73,10 +86,13 @@ void HomingMission::Init(TConfigurationNode& t_tree) {
 /****************************************/
 /****************************************/
 
-HomingMission::~HomingMission() {
-  if (m_bInitializedByARGoS) {
+HomingMission::~HomingMission()
+{
+  if (m_bInitializedByARGoS)
+  {
     delete m_pcHome;
-    for (size_t i = 0; i < m_vecDistractionPatches.size(); i++) {
+    for (size_t i = 0; i < m_vecDistractionPatches.size(); i++)
+    {
       delete m_vecDistractionPatches[i];
     }
   }
@@ -90,15 +106,16 @@ void HomingMission::Destroy() {}
 /****************************************/
 /****************************************/
 
-argos::CColor HomingMission::GetFloorColor(const argos::CVector2& c_position_on_plane) {
+argos::CColor HomingMission::GetFloorColor(const argos::CVector2 &c_position_on_plane)
+{
   return GenericLoopFunctions::GetFloorColor(c_position_on_plane);
 }
 
-
 /****************************************/
 /****************************************/
 
-void HomingMission::Reset() {
+void HomingMission::Reset()
+{
   GenericLoopFunctions::Reset();
   m_unRobotsAtHome = 0;
   m_unAnyTimeScore = 0;
@@ -107,20 +124,28 @@ void HomingMission::Reset() {
 /****************************************/
 /****************************************/
 
-void HomingMission::PostStep() {
+void HomingMission::PostStep()
+{
   m_unRobotsAtHome = 0;
 
-  CSpace::TMapPerType& tEpuckMap = GetSpace().GetEntitiesByType("epuck");
-  CVector2 cEpuckPosition(0,0);
-  for (CSpace::TMapPerType::iterator it = tEpuckMap.begin(); it != tEpuckMap.end(); ++it) {
-    CEPuckEntity* pcEpuck = any_cast<CEPuckEntity*>(it->second);
+  CSpace::TMapPerType &tEpuckMap = GetSpace().GetEntitiesByType("epuck");
+  CVector2 cEpuckPosition(0, 0);
+  for (CSpace::TMapPerType::iterator it = tEpuckMap.begin(); it != tEpuckMap.end(); ++it)
+  {
+    CEPuckEntity *pcEpuck = any_cast<CEPuckEntity *>(it->second);
 
     std::string strRobotId = pcEpuck->GetId();
     cEpuckPosition.Set(pcEpuck->GetEmbodiedEntity().GetOriginAnchor().Position.GetX(),
                        pcEpuck->GetEmbodiedEntity().GetOriginAnchor().Position.GetY());
 
+    std::cout << "%! id:" << count;
+    std::cout << " x:" << pcEpuck->GetEmbodiedEntity().GetOriginAnchor().Position.GetX();
+    std::cout << " y:" << pcEpuck->GetEmbodiedEntity().GetOriginAnchor().Position.GetY() << std::endl;
+    count++;
+
     // Check if robot on home patch
-    if (m_pcHome->IsOnPatch(cEpuckPosition)) {
+    if (m_pcHome->IsOnPatch(cEpuckPosition))
+    {
       m_unRobotsAtHome += 1;
     }
   }
@@ -131,11 +156,15 @@ void HomingMission::PostStep() {
 /****************************************/
 /****************************************/
 
-Real HomingMission::GetScore() {
+Real HomingMission::GetScore()
+{
   UInt32 unScore = 0;
-  if (m_strTypeObjectiveFunction == "'anytime'") {
+  if (m_strTypeObjectiveFunction == "'anytime'")
+  {
     unScore = m_unAnyTimeScore;
-  } else if (m_strTypeObjectiveFunction == "'endtime'") {
+  }
+  else if (m_strTypeObjectiveFunction == "'endtime'")
+  {
     unScore = m_unRobotsAtHome;
   }
   return unScore;
@@ -144,14 +173,16 @@ Real HomingMission::GetScore() {
 /****************************************/
 /****************************************/
 
-void HomingMission::PostExperiment() {
+void HomingMission::PostExperiment()
+{
   LOG << "Score " << GetScore() << std::endl;
 }
 
 /****************************************/
 /****************************************/
 
-void HomingMission::Initialize() {
+void HomingMission::Initialize()
+{
   GenericLoopFunctions::Initialize();
 
   std::map<std::string, argos::CAny>::iterator it;
@@ -161,13 +192,18 @@ void HomingMission::Initialize() {
 
   it = GetVariableFromDictionary("colorHome");
   std::string strColorHome = argos::any_cast<std::string>(it->second);
-  if (strColorHome == "'white'") {
+  if (strColorHome == "'white'")
+  {
     m_cColorHome = argos::CColor::WHITE;
     m_cColorDistractions = argos::CColor::BLACK;
-  } else if (strColorHome == "'black'") {
+  }
+  else if (strColorHome == "'black'")
+  {
     m_cColorHome = argos::CColor::BLACK;
     m_cColorDistractions = argos::CColor::WHITE;
-  } else {
+  }
+  else
+  {
     std::cout << "\033[1;31m" << "Error: unknown color nest \"" << strColorHome << "\" ." << "\033[0m" << std::endl;
     std::exit(1);
   }
@@ -175,18 +211,22 @@ void HomingMission::Initialize() {
   // Setting up patches. The first one is the Home, the others are distractions
   it = GetVariableFromDictionary("numberPatches");
   unsigned int unNumberPatches = stoi(argos::any_cast<std::string>(it->second));
-  std::vector<Patch*> vecPossibleAggregationPatches = m_pcArena->GetPossibleAggregationPatches();
-  std::vector<Patch*> vecAllPossiblePatches = m_pcArena->GetAllPossiblePatches();
+  std::vector<Patch *> vecPossibleAggregationPatches = m_pcArena->GetPossibleAggregationPatches();
+  std::vector<Patch *> vecAllPossiblePatches = m_pcArena->GetAllPossiblePatches();
   unsigned int iPatch = 0;
   unsigned int unRandomIndex;
-  while (iPatch < unNumberPatches) {
-    Patch* pcNewPatch;
-    if (iPatch == 0) {
+  while (iPatch < unNumberPatches)
+  {
+    Patch *pcNewPatch;
+    if (iPatch == 0)
+    {
       unRandomIndex = rand() % vecPossibleAggregationPatches.size();
       pcNewPatch = vecPossibleAggregationPatches[unRandomIndex];
       pcNewPatch->SetIndex(m_vecAllPatches.size());
       pcNewPatch->SetColor(m_cColorHome);
-    } else {
+    }
+    else
+    {
       unRandomIndex = rand() % vecAllPossiblePatches.size();
       pcNewPatch = vecAllPossiblePatches[unRandomIndex];
       pcNewPatch->SetIndex(m_vecAllPatches.size());
@@ -194,12 +234,16 @@ void HomingMission::Initialize() {
     }
 
     // Add patch to list of patches if no overlap with existing ones
-    if (!(m_pcArena->ArePatchesOverlapping(m_vecAllPatches, pcNewPatch))) {
+    if (!(m_pcArena->ArePatchesOverlapping(m_vecAllPatches, pcNewPatch)))
+    {
       m_vecAllPatches.push_back(pcNewPatch);
-      if (iPatch == 0) {
+      if (iPatch == 0)
+      {
         m_pcHome = pcNewPatch;
         vecPossibleAggregationPatches.erase(vecPossibleAggregationPatches.begin() + unRandomIndex);
-      } else {
+      }
+      else
+      {
         m_vecDistractionPatches.push_back(pcNewPatch);
         vecAllPossiblePatches.erase(vecAllPossiblePatches.begin() + unRandomIndex);
       }
@@ -211,7 +255,8 @@ void HomingMission::Initialize() {
 /****************************************/
 /****************************************/
 
-std::string HomingMission::GetLowLevelDescription() {
+std::string HomingMission::GetLowLevelDescription()
+{
   std::string strGenericPart = GenericLoopFunctions::GetLowLevelDescription();
   std::ostringstream cStream;
   cStream << strGenericPart;
@@ -219,7 +264,8 @@ std::string HomingMission::GetLowLevelDescription() {
   cStream << "--colhome " << m_cColorHome << " ";
   cStream << "--coldistration " << m_cColorDistractions << " ";
   cStream << "--np " << m_vecAllPatches.size() << " ";
-  for (size_t i = 0; i < m_vecAllPatches.size(); ++i) {
+  for (size_t i = 0; i < m_vecAllPatches.size(); ++i)
+  {
     cStream << m_vecAllPatches[i]->GetLowLevelDescription();
   }
   return cStream.str();
@@ -228,10 +274,13 @@ std::string HomingMission::GetLowLevelDescription() {
 /****************************************/
 /****************************************/
 
-float HomingMission::GetMaxValue(std::vector<float> vec_values) {
+float HomingMission::GetMaxValue(std::vector<float> vec_values)
+{
   float fLargestElement = vec_values[0];
-  for (std::vector<float>::iterator it = vec_values.begin(); it < vec_values.end(); it++) {
-    if (*it > fLargestElement) {
+  for (std::vector<float>::iterator it = vec_values.begin(); it < vec_values.end(); it++)
+  {
+    if (*it > fLargestElement)
+    {
       fLargestElement = *it;
     }
   }
